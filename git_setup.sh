f88c7a7c83f33e93a0c10150bd2c18916dd57a1a
Gabor Kapitany <captaingabi@gmail.com>
2022-10-26 13:05:42 +0200

    ci: fix ansible-lint .cache and .ansible directories
    Change-Id: I16eadf32112f7827f62533e0a2b9cd57b3e8ac88

--- git_setup.sh
+++ git_setup.sh
@@ -6,7 +6,7 @@
 TOP_DIR="$(git rev-parse --show-toplevel)"
 
 # install post-commit hook locally.
-cp --remove-destination \
+ln -s -f \
     "${TOP_DIR}"/ci/post-commit-hook.sh \
     "$(git rev-parse --git-dir)"/hooks/post-commit
 
