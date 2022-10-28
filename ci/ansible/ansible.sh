e307647595bf22b862b54415e47bd998e732f1ef
Gabor Kapitany <captaingabi@gmail.com>
2022-10-28 09:45:33 +0200

    ci: add explicit dns when running ansible container
    Change-Id: If57856912465a3e39f98f86908d2faf2509222ea

--- ci/ansible/ansible.sh
+++ ci/ansible/ansible.sh
@@ -11,6 +11,7 @@ source "${TOP_DIR}"/packaging/common/docker_images.inc.sh
 
 docker run --rm -it \
     --workdir /workdir \
+    --dns 8.8.8.8 --dns 8.8.4.4 \
     --volume "${VAULT_ID_FILE}":/"${VAULT_ID_FILE}" \
     --volume "${PWD}":/workdir "${DOCKER_ANSIBLE_IMAGE}" \
     "$@"
